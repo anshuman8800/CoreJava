deefdc55da565b014174674caeec9f834208ef4c
Anshuman Kumar <anshuman.kumar@314ecorp.com>
2023-07-20 16:26:42 +0530

    Add use case of ArrayList in java

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,36 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+record GroceryItem(String name, String type, int count) {
+    public GroceryItem(String name) {
+        this(name, "DAIRY", 1);
+    }
+    @Override
+    public String toString() {
+        return String.format("%d %s in %s", count, name.toUpperCase(), type);
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        Car car = new Car(8, "Base car");
-        System.out.println(car.startEngine());
-        System.out.println(car.accelerate());
-        System.out.println(car.brake());
 
-        Mitsubishi mitsubishi = new Mitsubishi(6, "Outlander VRX 4WD");
-        System.out.println(mitsubishi.startEngine());
-        System.out.println(mitsubishi.accelerate());
-        System.out.println(mitsubishi.brake());
+        GroceryItem[] groceryArray = new GroceryItem[3];
+        groceryArray[0] = new GroceryItem("milk");
+        groceryArray[1] = new GroceryItem("apples", "PRODUCE", 6);
+        groceryArray[2] = new GroceryItem("oranges", "PRODUCE", 5);
+        System.out.println(Arrays.toString(groceryArray));
 
-        Ford ford = new Ford(6, "Ford Falcon");
-        System.out.println(ford.startEngine());
-        System.out.println(ford.accelerate());
-        System.out.println(ford.brake());
+        ArrayList objectList = new ArrayList();
+        objectList.add(new GroceryItem("Butter"));
+        objectList.add("Yogurt");
 
-        Holden holden = new Holden(6, "Holden Commodore");
-        System.out.println(holden.startEngine());
-        System.out.println(holden.accelerate());
-        System.out.println(holden.brake());
+        ArrayList<GroceryItem> groceryList = new ArrayList<>();
+        groceryList.add(new GroceryItem("Butter"));
+        groceryList.add(new GroceryItem("milk"));
+        groceryList.add(new GroceryItem("oranges", "PRODUCE", 5));
+        groceryList.set(0,
+                new GroceryItem("apples", "PRODUCE", 6));
+        groceryList.remove(1);
+        System.out.println(groceryList);
     }
 }
