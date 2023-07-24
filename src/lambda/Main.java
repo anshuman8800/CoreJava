6c59cd58ae3b8c9f6e1d7978964bc659c23b06e2
Anshuman Kumar <anshuman.kumar@314ecorp.com>
2023-07-24 18:31:09 +0530

    Create EnhanceComparator which extend the Comparator class and perform Sorting

--- src/lambda/Main.java
+++ src/lambda/Main.java
@@ -2,6 +2,7 @@ package lambda;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
@@ -18,7 +19,8 @@ public class Main {
                             new Person("Anshuman", "Kumar"),
                             new Person("Satwik", "Kumar"),
                             new Person("Anmol", "Singh"),
-                            new Person("Rakesh", "Anna")
+                            new Person("Rakesh", "Anna"),
+                            new Person("Amitesh", "Anna")
                     )
             );
 
@@ -36,6 +38,28 @@ public class Main {
             // using lambda expression
             people.sort((o1, o2) -> o1.lastName.compareTo(o2.lastName));
             System.out.println(people);
+
+            interface EnhanceComparator<T> extends Comparator<T> {
+                int secondLevel(T o1, T o2);
+            }
+
+            var comparatorMixed = new EnhanceComparator<Person>() {
+
+                @Override
+                public int compare(Person o1, Person o2) {
+                    int result = o1.lastName.compareTo(o2.lastName);
+                    return result == 0 ? secondLevel(o1, o2) : result;
+                }
+
+                @Override
+                public int secondLevel(Person o1, Person o2) {
+                    return o1.firstName.compareTo(o2.firstName);
+                }
+            };
+
+            people.sort(comparatorMixed);
+            System.out.println(people);
+
         }
     }
 }
