db6fc5f64dfed323bc618fb369f123377f9a6c67
Anshuman Kumar <anshuman.kumar@314ecorp.com>
2023-07-31 15:07:40 +0530

    Run multiple thread parallelly

--- src/thread/Main.java
+++ src/thread/Main.java
@@ -5,9 +5,35 @@ public class Main {
         System.out.println("Main Thread");
         Thread newThread = new BasicThread();
         newThread.start();
-
+        // Don't call the run method directly, call start method
         MyRunnable newMyRunnable = new MyRunnable();
         Thread newThreadFromRunnableClass = new Thread(newMyRunnable);
         newThreadFromRunnableClass.start();
+        Thread tempThread = new Thread(() -> {
+            int i = 0;
+            while(true) {
+                System.out.println("i " + i);
+                i++;
+            }
+        });
+
+        Thread tempThread1 = new Thread(() -> {
+            int i = 0;
+            while(true) {
+                System.out.println("j " + i);
+                i++;
+            }
+        });
+
+        Thread tempThread2 = new Thread(() -> {
+            int i = 0;
+            while(true) {
+                System.out.println("k " + i);
+                i++;
+            }
+        });
+        tempThread.start();
+        tempThread2.start();
+        tempThread1.start();
     }
 }
